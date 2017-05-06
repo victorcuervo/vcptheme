5485bbc179fcd762b72104e0a491b98d075a8d31
victorcuervo <vcuervo@gmail.com>
2017-05-06 19:32:30 +0200

    Ajustada plantilla home

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template Name: Home Page
- * Plantilla para la p�gina principal
+ * Plantilla para la página principal
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
@@ -12,11 +12,11 @@ get_header(); ?>
 
 <div id="cuerpo" class="container">
 
-		
+
 
 			<?php if ( have_posts() ) : the_post(); the_content();?>
 
-	
+
 		<div class="headline">
 			<h2>Últimos Artículos</h2>
 		</div>
@@ -25,26 +25,26 @@ get_header(); ?>
 		<?php
 
 			function get_excerpt_by_id2($post_id){
-			$the_post = get_post($post_id); //Gets post ID
-			$the_excerpt = $the_post->post_content; //Gets post_content to be used as a basis for the excerpt
-			$excerpt_length = 20; //Sets excerpt length by word count
-			$the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
-			$words = explode(' ', $the_excerpt, $excerpt_length + 1);
-			if(count($words) > $excerpt_length) :
-			array_pop($words);
-			array_push($words, '…');
-			$the_excerpt = implode(' ', $words);
-			endif;
-			$the_excerpt = '<p>' . $the_excerpt . '</p>';
-			return $the_excerpt;
-		}
-		
+				$the_post = get_post($post_id); //Gets post ID
+				$the_excerpt = $the_post->post_content; //Gets post_content to be used as a basis for the excerpt
+				$excerpt_length = 20; //Sets excerpt length by word count
+				$the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
+				$words = explode(' ', $the_excerpt, $excerpt_length + 1);
+				if(count($words) > $excerpt_length) :
+				array_pop($words);
+				array_push($words, '…');
+				$the_excerpt = implode(' ', $words);
+				endif;
+				$the_excerpt = '<p>' . $the_excerpt . '</p>';
+				return $the_excerpt;
+			}
+
 		$category = get_queried_object();
 
 		$recent_posts = wp_get_recent_posts(array('numberposts' => '6', 'post_status' => 'publish'));
 		foreach( $recent_posts as $recent ){
-			
-		
+
+
 
 			?>
 			<div class="col-md-4 col-sm-6">
@@ -56,7 +56,7 @@ get_header(); ?>
 				  </a>
 				  <div class="media-body">
 				    <h4 class="media-heading"><?php echo '<a href="' . get_permalink($recent["ID"]) . '" title="'.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a>'?></h4>
-					    
+
 					    <?php echo get_excerpt_by_id2($recent["ID"]); ?>
 
 			      </div>
@@ -106,8 +106,8 @@ get_header(); ?>
 
 		</div>
 
-			
-				
+
+
 
 
 
@@ -134,7 +134,7 @@ get_header(); ?>
 
 				endif;
 			?>
-	
+
 </div><!-- #Fin Contenido Principal -->
 
 <?php

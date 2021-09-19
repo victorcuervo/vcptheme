f6b95f6730a309dbaefba3cfdc962ad4c3d29fc3
victorcuervo <vcuervo@gmail.com>
2021-09-19 21:34:06 +0200

    Quitar jQuery Migrate

--- functions.php
+++ functions.php
@@ -908,3 +908,19 @@ function remove_cssjs_ver( $src ) {
 }
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+
+/* Quitar jQuery Migrate */
+function remove_jquery_migrate($scripts){
+	if (!is_admin() && isset($scripts->registered['jquery'])) {
+	   $script = $scripts->registered['jquery'];
+					 
+	   if ($script->deps) {
+		  // Check whether the script has any dependencies
+		  $script->deps = array_diff($script->deps, array(
+			 'jquery-migrate'
+		  ));
+	   }
+	}
+ }
+ add_action('wp_default_scripts', 'remove_jquery_migrate');
